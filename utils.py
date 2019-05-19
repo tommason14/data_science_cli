f984640c4537c7d889660c7e2e6c7dee09034735
Tom Mason <tommason14@gmail.com>
2019-05-19 13:57:34 +1000

    parse_df: select columns, filter works

--- utils.py
+++ utils.py
@@ -1,13 +1,37 @@
+import pandas as pd
+
 def data_from_stdin():
     """
     Retuns a dataframe from a csv file piped in from stdin.
     """
-    import pandas as pd
     import sys
     data = [line.strip() for line in sys.stdin]
     data = [line.split(',') for line in data]
     header, *data = data
     indexes = [i for i in range(len(data))]
     df = pd.DataFrame(data, columns = header, index = indexes)
+    # need to make sure columns are correct type
+    df = convert_col_to_numeric(df)
     return df
 
+def convert_col_to_numeric(df):
+    """
+    Checks if every item in the column can be converted to
+    a numeric type.
+    If yes, converts to numeric.
+    Necessary as reading from stdin creates strings
+    """
+    # using pandas apply
+    for col in df.columns:
+        results = []
+        numeric = False
+        for value in df[col]:
+            try:
+                value = float(value)
+                numeric = True
+            except ValueError:
+                pass
+            results.append(numeric)
+        if all(item for item in results): 
+            df[col] = pd.to_numeric(df[col])
+    return df
